29de3b60533271468069630067c9e2005aca0be5
1216692740 <104670904+1216692740@users.noreply.github.com>
2023-01-12 13:45:58 +0800

    添加微信

---
+++ Apps/微信/不清理安装包/wechat.sh
@@ -0,0 +1,1 @@
+sudo wget https://archive.ubuntukylin.com/software/pool/partner/weixin_2.1.1_amd64.deb && sudo apt install ./weixin_2.1.1_amd64.deb -y
---
+++ Apps/微信/清理安装包/wechat.sh
@@ -0,0 +1,2 @@
+sudo wget https://archive.ubuntukylin.com/software/pool/partner/weixin_2.1.1_amd64.deb && sudo apt install ./weixin_2.1.1_amd64.deb -y
+sudo rm ./weixin_2.1.1_amd64.deb
