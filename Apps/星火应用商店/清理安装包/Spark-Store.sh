e780af48f50c2afeb66345f80bab1079d5d8641f
1216692740 <104670904+1216692740@users.noreply.github.com>
2023-01-12 12:52:05 +0800

    添加软件

--- Apps/星火应用商店/清理安装包/Spark-Store.sh
+++ Apps/星火应用商店/清理安装包/Spark-Store.sh
@@ -1,2 +1,2 @@
-wget https://gitee.com/deepin-community-store/spark-store/releases/download/4.1.2/spark-store_4.1.2_amd64.deb && sudo apt install ./spark-store_4.1.2_amd64
-rm ./spark-store_4.1.2_amd64
+wget https://gitee.com/deepin-community-store/spark-store/releases/download/4.1.2/spark-store_4.1.2_amd64.deb && sudo apt install ./spark-store_4.1.2_amd64.deb
+rm ./spark-store_4.1.2_amd64.deb
