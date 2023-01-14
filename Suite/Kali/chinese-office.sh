49a37e09af7ae0b723aba31b25e127bbe4bd1fb0
1216692740 <104670904+1216692740@users.noreply.github.com>
2023-01-14 12:38:45 +0800

    添加套件安装’

---
+++ Suite/Kali/chinese-office.sh
@@ -0,0 +1,11 @@
+wget https://ghproxy.com/https://raw.githubusercontent.com/1216692740/Fast-Install-Chinese-Debian-Apps/main/Apps/WPS%20Office/%E6%B8%85%E7%90%86%E5%AE%89%E8%A3%85%E5%8C%85/WPS-Office.sh
+bash ./WPS-Office.sh
+wget https://ghproxy.com/https://raw.githubusercontent.com/1216692740/Fast-Install-Chinese-Debian-Apps/main/Apps/%E5%BE%AE%E4%BF%A1/%E6%B8%85%E7%90%86%E5%AE%89%E8%A3%85%E5%8C%85/wechat.sh
+bash ./wechat.sh
+wget https://ghproxy.com/https://raw.githubusercontent.com/1216692740/Fast-Install-Chinese-Debian-Apps/main/Apps/QQ/%E6%B8%85%E7%90%86%E5%AE%89%E8%A3%85%E5%8C%85/QQ.sh
+bash ./QQ.sh
+wget https://ghproxy.com/https://raw.githubusercontent.com/1216692740/Fast-Install-Chinese-Debian-Apps/main/Apps/%E6%98%9F%E7%81%AB%E5%BA%94%E7%94%A8%E5%95%86%E5%BA%97/%E6%B8%85%E7%90%86%E5%AE%89%E8%A3%85%E5%8C%85/Spark-Store.sh
+bash ./Spark-Store.sh
+wget https://ghproxy.com/https://raw.githubusercontent.com/1216692740/Fast-Install-Chinese-Debian-Apps/main/Apps/360%E5%AE%89%E5%85%A8%E6%B5%8F%E8%A7%88%E5%99%A8/%E6%B8%85%E7%90%86%E5%AE%89%E8%A3%85%E5%8C%85/360se.sh
+bash ./360se.sh
+echo 安装完成
