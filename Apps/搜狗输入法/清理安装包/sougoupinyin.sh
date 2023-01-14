6c3c48b5549bfff63ff600dfc5bfad4ec244108b
BlockHaity <104670904+1216692740@users.noreply.github.com>
2023-01-14 18:29:48 +0800

    添加搜狗输入法

---
+++ Apps/搜狗输入法/清理安装包/sougoupinyin.sh
@@ -0,0 +1,4 @@
+sudo wget https://ime.sogouimecdn.com/202301141824/7b9282cf2adc43d6b029257177abec05/dl/gzindex/1656597217/sogoupinyin_4.0.1.2800_x86_64.deb
+sudo apt install ./sogoupinyin_4.0.1.2800_x86_64.deb
+im-config -s fcitx -z default
+sudo rm ./sogoupinyin_4.0.1.2800_x86_64.deb
