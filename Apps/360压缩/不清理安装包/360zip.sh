4711958e55fd3eda6457949288f5f5577a9419c4
BlockHaity <104670904+1216692740@users.noreply.github.com>
2023-01-14 17:19:29 +0800

    添加60压缩

---
+++ Apps/360压缩/不清理安装包/360zip.sh
@@ -0,0 +1,2 @@
+sudo wget https://packages.deepin.com/deepin/pool/non-free/3/360zip/360zip_1.0.0.1010_amd64.deb
+sudo apt install ./360zip_1.0.0.1010_amd64.deb
---
+++ Apps/360压缩/清理安装包/360zip.sh
@@ -0,0 +1,3 @@
+sudo wget https://packages.deepin.com/deepin/pool/non-free/3/360zip/360zip_1.0.0.1010_amd64.deb
+sudo apt install ./360zip_1.0.0.1010_amd64.deb
+sudo rm ./360zip_1.0.0.1010_amd64.deb
