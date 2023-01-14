8609bb42616fd90b8c1b49d8628a4050c8f4da3b
BlockHaity <104670904+1216692740@users.noreply.github.com>
2023-01-14 15:38:50 +0800

    添加Chrome

---
+++ Apps/Chrome/不清理安装包/Chrome.sh
@@ -0,0 +1,1 @@
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt install ./google-chrome-stable_current_amd64.deb
---
+++ Apps/Chrome/清理安装包/Chrome.sh
@@ -0,0 +1,2 @@
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt install ./google-chrome-stable_current_amd64.deb
+sudo rm ./google-chrome-stable_current_amd64.deb
