63b2521684fd75f40e5760d747e9eed26b2b2f14
1216692740 <104670904+1216692740@users.noreply.github.com>
2023-01-14 14:28:57 +0800

    添加Edge

---
+++ Apps/Edge浏览器/清理安装包/Edge.sh
@@ -0,0 +1,3 @@
+sudo wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_109.0.1518.52-1_amd64.deb
+sudo apt install ./microsoft-edge-stable_109.0.1518.52-1_amd64.deb
+rm ./microsoft-edge-stable_109.0.1518.52-1_amd64.deb
