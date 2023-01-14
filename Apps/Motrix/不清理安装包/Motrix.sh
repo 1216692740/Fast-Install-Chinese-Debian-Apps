cb805bc97c1a28493958a454154f1f6803347f9a
BlockHaity <104670904+1216692740@users.noreply.github.com>
2023-01-14 17:02:26 +0800

    添加Motrix

---
+++ Apps/Motrix/不清理安装包/Motrix.sh
@@ -0,0 +1,2 @@
+sudo wget https://ghproxy.com/https://github.com/agalwood/Motrix/releases/download/v1.6.11/Motrix_1.6.11_amd64.deb
+sudo apt install ./Motrix_1.6.11_amd64.deb
---
+++ Apps/Motrix/清理安装包/Motrix.sh
@@ -0,0 +1,3 @@
+sudo wget https://ghproxy.com/https://github.com/agalwood/Motrix/releases/download/v1.6.11/Motrix_1.6.11_amd64.deb
+sudo apt install ./Motrix_1.6.11_amd64.deb
+sudo rm ./Motrix_1.6.11_amd64.deb
